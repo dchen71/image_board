544a8f022ccd13d404359359986e5634a1fa2174
Dan <dchen12812@gmail.com>
2015-08-02 17:54:11 -0700

    fixed user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,20 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def show
+		@user = User.find_by(id: params[:id])
+		@image = Image.new
+		@images = @user.images
+		@tag = Tag.new
+		@tags = Set.new
+		@images.each do |i|
+			i.tags.each do |j|
+				@tags.add(j)
+			end
+		end
+		@tags = @tags.take(10)
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:username, :password, :password_confirmation, :email)

2b0ed13bf07074ff4ec857cc64137840e2370ade
Dan <dchen12812@gmail.com>
2015-08-02 17:46:01 -0700

    changed ui to unify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :images
+  has_many :images, dependent: :destroy
 
   validates_presence_of :username,:password_digest, :email
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }

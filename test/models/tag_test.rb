7c198d5ddf338aa3c9c66c09806db2800fe86ce4
Dan <dchen12812@gmail.com>
2015-08-27 01:03:05 -0700

    tag model tests

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@tag = Tag.new(tag: 'qoo')
+  end
+
+  test 'should be valid' do
+  	assert @tag.valid?
+  end
+
+  test 'tag should be present' do
+  	@tag.tag = ''
+  	assert_not @tag.valid?
+  end
+
+  test 'tag max length of 30' do
+  	@tag.tag = 'c' * 31
+  	assert_not @tag.valid?
+  end
+
+  test ' tag must be unique' do
+  	@tag.save
+  	@newtag = Tag.new(tag: 'qoo')
+  	assert_not @newtag.valid?
+  end
 end

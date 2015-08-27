4b61526b7955dd36b655a1b4452e079885ae7066
Dan <dchen12812@gmail.com>
2015-08-27 01:41:09 -0700

    comment model tests

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@comment = Comment.new(content: 'coo coo', image_id: 1, user_id: 1)
+  end
+
+  test 'should be valid' do
+  	assert @comment.valid?
+  end
+
+  test 'image_id must be present' do
+  	@comment.image_id = ''
+  	assert_not @comment.valid?
+  end
+
+  test 'content must be present' do
+  	@comment.content = ''
+  	assert_not @comment.valid?
+  end
+
+  test 'user_id must be present' do
+  	@comment.user_id = ''
+  	assert_not @comment.valid?
+  end
 end

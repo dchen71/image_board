b3ea5bf67dbac9b2fb14dcde95275941160ac4de
Dan <dchen12812@gmail.com>
2015-08-27 00:57:04 -0700

    added user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,31 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+  	@user = User.new(username: 'bob', password_digest: 'coolcool', email: 'bob@bob.com')
+  end
+
+  test 'user is valid' do
+  	assert @user.valid?
+  end
+
+  test 'username is needed' do
+  	@user.username = ' '
+  	assert_not @user.valid?
+  end
+
+  test 'password is needed' do
+  	@user.password_digest = ' '
+  	assert_not @user.valid?
+  end
+
+  test 'email is needed' do
+  	@user.email = ' '
+  	assert_not @user.valid?
+  end
+
+  test 'email must be email' do
+  	@user.email = 'catman'
+  	assert_not @user.valid?
+  end
 end

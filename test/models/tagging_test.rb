f389f13f2ebd7f506ba77008bafa0cee6936a034
Dan <dchen12812@gmail.com>
2015-08-27 01:36:28 -0700

    tagging model tests

--- test/models/tagging_test.rb
+++ test/models/tagging_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class TaggingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@tagging = Tagging.new(tag_id: 1, image_id: 1)
+  end
+
+  test 'taging should be valid' do
+  	assert @tagging.valid?
+  end
+
+  test 'tagging should be unique per image' do
+  	@tagging.save
+  	@new_tag = Tagging.new(tag_id: 1, image_id: 2)
+  	assert @new_tag.valid?
+  	@new_tag.image_id = 1
+  	assert_not @new_tag.valid?
+  end
+
 end

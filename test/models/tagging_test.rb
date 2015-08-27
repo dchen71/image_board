685199bcb4bb8af06e4dedf9390780fd8c148af9
Dan <dchen12812@gmail.com>
2015-08-27 02:10:01 -0700

    updated fixtures

--- test/models/tagging_test.rb
+++ test/models/tagging_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TaggingTest < ActiveSupport::TestCase
   def setup
-  	@tagging = Tagging.new(tag_id: 1, image_id: 1)
+  	@tagging = Tagging.new(tag_id: 5, image_id: 5)
   end
 
   test 'taging should be valid' do
@@ -11,9 +11,9 @@ class TaggingTest < ActiveSupport::TestCase
 
   test 'tagging should be unique per image' do
   	@tagging.save
-  	@new_tag = Tagging.new(tag_id: 1, image_id: 2)
+  	@new_tag = Tagging.new(tag_id: 5, image_id: 3)
   	assert @new_tag.valid?
-  	@new_tag.image_id = 1
+  	@new_tag.image_id = 5
   	assert_not @new_tag.valid?
   end
 

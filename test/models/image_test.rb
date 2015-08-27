d60854a7b96a128a819be5dcca18cb0ef358503e
Dan <dchen12812@gmail.com>
2015-08-27 01:30:56 -0700

    image model tests

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@image = Image.new(user_id: 1)
+  	@image.pictures = File.new('seed/k1.jpg')
+  end
+  
+  test 'should be valid' do
+  	assert @image.valid?
+  end
+
+  test 'user id must be present' do
+  	@image.user_id = ''
+  	assert_not @image.valid?
+  end
+
+  test 'pictures must be jpg/jpeg/png' do
+  	@image.pictures = File.new('test/fixtures/users.yml')
+  	assert_not @image.valid?
+  end
+
 end

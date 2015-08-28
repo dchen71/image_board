f81465c49d55affcf73864551299eb30c44353ba
Dan <dchen12812@gmail.com>
2015-08-27 20:09:09 -0700

    added user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = users(:user1)
+  end
+
+  test 'get new' do
+  	get :new
+  	assert_response :success
+  end
+
+  test 'post create' do
+  	post :create, user: {username: 'bob', email: 'cat@yahoo.com', password: 'password', password_confirmation: 'password'}
+  	assert_redirected_to root_path
+  end
+
+  test 'get show' do
+  	get :show, id: @user.id
+  	assert_response :success
+  end
 end

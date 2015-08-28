822d8927ffad95db6f7c04f5417b0ddfde857cfa
Dan <dchen12812@gmail.com>
2015-08-27 18:52:21 -0700

    updated image controller tests

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@image = images(:pic1)
+  	@user = users(:user1)
+  	@image.user_id = @user.id
+  	@image.save
+  end
+
+  test 'should redirect from new when not logged in' do
+  	get :new
+  	assert_redirected_to login_path
+  end
+
+  test 'should redirect from create when not logged in' do
+  	post :create
+  	assert_redirected_to login_path
+  end
+
+  test 'index should work' do
+  	get :index
+  	assert_response :success
+  end
+
+  test 'show should work' do
+  	get(:show, {id: @image.id})
+  	assert_response :success
+  end
 end

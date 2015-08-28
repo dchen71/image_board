7323f6adefa9a752c80d5037970914ae0ddcd7ea
Dan <dchen12812@gmail.com>
2015-08-27 17:39:23 -0700

    tags controller tests

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class TagsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@tag = tags(:tag1)
+  	@other = tags(:tag2)
+  	@image = images(:pic1)
+  end
+
+  test 'should redirect from create when not logged in' do
+  	post :create
+  	assert_redirected_to login_url
+  end
+
+  test 'search should work' do
+  	get :search, id: @tag.id
+  	assert_response :success
+  end
+
+  test 'should be able to use show' do
+  	get :show, id: @tag.id
+  	assert_response :success
+  end
 end
